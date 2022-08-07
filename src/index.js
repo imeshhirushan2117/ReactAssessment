294106c12fec4bef136f3ecfa586ca07645aba40
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2022-08-08 01:00:45 +0530

    update tailwind

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { StyledEngineProvider } from '@mui/material/styles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <StyledEngineProvider injectFirst>
+          <App />
+      </StyledEngineProvider>
   </React.StrictMode>
 );
 
